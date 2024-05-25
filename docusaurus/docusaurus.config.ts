1f3726334931baf3e04f7a35cd3621ff14494348
ianhanssoniohk <ian.hansson@iohk.io>
2024-05-25 15:44:39 +0100

    Deploy docusaurus pipeline

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -15,8 +15,11 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+
+  projectName: "ianhanssoniohk.github.io",
+  organizationName: "ianhanssoniohk",
+  deploymentBranch: "gh-pages",
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
